431d33ca50ba493f424e77b0c2c65603929508c5
Chris Pennington <chris@codinginpublic.dev>
2023-07-27 14:19:59 -0600

    install bookshop

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import image from "@astrojs/image";
@@ -6,10 +6,16 @@ import image from "@astrojs/image";
 // https://astro.build/config
 import sitemap from "@astrojs/sitemap";
 
+import bookshop from "@bookshop/astro-bookshop"; // add for bookshop
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-cip.netlify.app',
-  integrations: [image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), sitemap()]
+  site: "https://astro-blog-cip.netlify.app",
+  integrations: [
+    image({
+      serviceEntryPoint: "@astrojs/image/sharp",
+    }),
+    bookshop(), // add for bookshop
+    sitemap(),
+  ],
 });
